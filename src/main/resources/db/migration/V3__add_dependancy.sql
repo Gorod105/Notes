43de496fc14e89896987bee26aa7e22d30e920f9
Roman horodyskyi <gorod10516@gmail.com>
2024-09-11 10:48:47 +0300

    Send to check to LMS

--- src/main/resources/db/migration/V3__add_dependancy.sql
+++ src/main/resources/db/migration/V3__add_dependancy.sql
@@ -2,14 +2,3 @@ INSERT INTO users_roles (users_id, roles_id)
 VALUES (1,1),
        (2,2),
        (2,1);
-INSERT INTO notes (title, content)
-VALUES ('Title1', 'Content1'),
-       ('Title2', 'Content2'),
-       ('Title3', 'Content3'),
-       ('Title4', 'Content4'),
-       ('Title5', 'Content5'),
-       ('Title6', 'Content6'),
-       ('Title7', 'Content7'),
-       ('Title8', 'Content8'),
-       ('Title9', 'Content9'),
-       ('Title10', 'Content10');
